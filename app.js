9ecad1ae3086c0be5a8e50e23fec8d9472334286
Caleb Bartholomew <caleb.bartholomew@xi3.com>
2015-06-06 13:17:57 -0600

    Created star field banner to get in the proper mood for writing Star Wars things.

--- app.js
+++ app.js
@@ -9,16 +9,56 @@
 
 var express = require('express');
 var app     = express();
+var colors  = require('colors');
 
 //
 // Configure Express
 //
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT     || 3000);
+app.set('env',  process.env.NODE_ENV || 'development')
 
 
 //
 // Start listening for connections
 //
 app.listen(app.get('port'), function() {
-	console.log('Server listen on port: ' + app.get('port'));
+	/**
+	  Creates a simple star filed :)
+	 */
+	function CreateStarField(rows, columns, chance) {
+		var stars = ['˙','¨','᛫','．','.','・','˙','¨','᛫','⁖','．','・','・','˙','¨','᛫','．','・','•','・','・','・','・',];
+
+		for(var n = 0; n < rows; ++n) {
+			for(var i = 0; i < columns; ++i) {
+				if(Math.random() * 100 > chance) {
+					var s = Math.floor(Math.random() * stars.length);
+					process.stdout.write(stars[s]);
+				} else {
+					process.stdout.write(' ');
+				}
+			}
+			console.log('');
+		}
+	}
+
+	/**
+	  Logs a string at a relative y value, and an absolute x value.
+	 */
+	function logStringAt(string, x, y) {
+		process.stdout.moveCursor(0, y);
+		process.stdout.cursorTo(x);
+		process.stdout.write(string);
+	}
+
+	CreateStarField(11, 50, 85);
+
+	logStringAt('╒══════════════════════════════════╕', 8, -9);
+	logStringAt('│       Star Wars API Viewer       │', 8, 1);
+	logStringAt('└──────────────────────────────────┘', 8, 1);
+	logStringAt('  Server listening for connections    ', 8, 2);
+	logStringAt('    Port:        ' + app.get('port').toString().cyan.bold, 8, 1);
+	logStringAt('    Environment: ' + app.get('env').toString().cyan.bold, 8, 1);
+
+	logStringAt('\n', 0, 4);
+
 });
